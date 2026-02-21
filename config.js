ed133ea98357a7220ea66ad68a10b3a074915b0c
TonCrimeGame-dotcom <yagizozguney@gmail.com>
2026-02-21 14:48:13 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+/* ========= GLOBAL CONFIG ========= */
+
+window.CONFIG = {
+
+SUPABASE_URL:
+"https://hwhscuyudwphnsipibpy.supabase.co",
+
+SUPABASE_KEY:
+"sb_publishable_dItLcV8z83CvDWuR8nTabA_ImTHGETu",
+
+USER_ID:"591676206",
+
+MAX_ENERGY:100,
+XP_LIMIT:100,
+ENERGY_INTERVAL:5*60*1000
+
+};
