73277d187bc059efb5b10ee9c14e58e0b25ddc5c
TonCrimeGame-dotcom <yagizozguney@gmail.com>
2026-03-02 15:25:11 +0300

    Update Assets.js

--- engine/Assets.js
+++ engine/Assets.js
@@ -1,26 +1,15 @@
 export class Assets {
   constructor() {
-    this.images = new Map();
+    this.images = {};
   }
 
-  async loadImages(list) {
-    const tasks = list.map(({ key, src }) => this._loadImage(key, src));
-    await Promise.all(tasks);
+  image(key, src) {
+    const img = new Image();
+    img.src = src;
+    this.images[key] = img;
   }
 
   getImage(key) {
-    return this.images.get(key) || null;
-  }
-
-  _loadImage(key, src) {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.onload = () => {
-        this.images.set(key, img);
-        resolve();
-      };
-      img.onerror = () => reject(new Error(`Image load failed: ${src}`));
-      img.src = src;
-    });
+    return this.images[key];
   }
 }
