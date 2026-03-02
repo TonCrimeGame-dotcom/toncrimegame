c17c38d2c9ffbe9bd9cbfd61d3b8ef82d5b6541b
TonCrimeGame-dotcom <yagizozguney@gmail.com>
2026-03-02 15:45:55 +0300

    Update Assets.js

--- engine/Assets.js
+++ engine/Assets.js
@@ -1,15 +1,37 @@
 export class Assets {
   constructor() {
-    this.images = {};
+    this.images = new Map();
+    this.promises = [];
   }
 
+  // ✅ main.js adaptörü bu 3 ismi de arıyor, hepsini destekleyelim:
   image(key, src) {
+    return this.loadImage(key, src);
+  }
+
+  addImage(key, src) {
+    return this.loadImage(key, src);
+  }
+
+  loadImage(key, src) {
     const img = new Image();
+    const p = new Promise((resolve, reject) => {
+      img.onload = () => resolve(img);
+      img.onerror = (e) => reject(e);
+    });
     img.src = src;
-    this.images[key] = img;
+
+    this.images.set(key, img);
+    this.promises.push(p);
+    return img;
   }
 
   getImage(key) {
-    return this.images[key];
+    return this.images.get(key);
+  }
+
+  // (Opsiyonel) preload bitmesini beklemek istersen
+  async ready() {
+    await Promise.allSettled(this.promises);
   }
 }
